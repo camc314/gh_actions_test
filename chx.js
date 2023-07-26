690250e4030cd268d2fd57c8cd9285f5b68581a1
Cameron Clark <cameron.clark@hey.com>
2023-07-26 09:43:01 +0100

    test

--- chx.js
+++ chx.js
@@ -1,6 +1,5 @@
 const fetch = require("node-fetch");
 const core = require("@actions/core");
-const github = require("@actions/github");
 
 async function checkBranchExistence(owner, repo, branchLabel, token) {
     const url = `https://api.github.com/repos/${owner}/${repo}/branches/${branchLabel}`;
@@ -23,80 +22,66 @@ async function checkBranchExistence(owner, repo, branchLabel, token) {
     throw new Error(`Unexpected response status ${response.status}`);
 }
 
-// read new line separated labels from stdin
-function readFromStdin() {
-    return new Promise((resolve, reject) => {
-        let data = "";
+/**
+ *
+ * @returns {string[]} The labels for the current PR
+ */
+function getPrLabels() {
+    let labels = [];
 
-        process.stdin.on("readable", () => {
-            let chunk;
-            while ((chunk = process.stdin.read()) !== null) {
-                data += chunk;
-            }
-        });
+    const processEnvPrLabels = process.env.CURRENT_PR_LABELS;
 
-        process.stdin.on("end", () => {
-            resolve(data);
-        });
+    if (!processEnvPrLabels) {
+        throw new Error("No labels found in process.env.CURRENT_PR_LABELS");
+    }
 
-        process.stdin.on("error", (err) => {
-            reject(err);
-        });
-    });
+    try {
+        labels = JSON.parse(processEnvPrLabels);
+    } catch (error) {
+        throw new Error(
+            `Error parsing process.env.CURRENT_PR_LABELS: ${error}`
+        );
+    }
+
+    return labels;
 }
 
 (async function () {
-    // core.
+    const prLabels = getPrLabels();
 
-    // github.context.even;
-
-    console.warn("START SCRIPT");
-    console.log(
-        "process.env.CURRENT_PR_LABELS: '" + process.env.CURRENT_PR_LABELS + "'"
-    );
-    console.log(JSON.parse(process.env.CURRENT_PR_LABELS));
     const owner = "openfin";
     const repo = process.argv[2];
+    const token = process.argv[2];
 
-    const stdin = await readFromStdin();
-    console.log(`Stdin: "${stdin}"`);
-    console.log(`Stdin: "${JSON.parse(stdin)}"`);
-
-    // log stdin as char: charcode
-    console.log("~~~~~~~~~~~ STDIN CHARCODES ~~~~~~~~~~~");
-    for (let i = 0; i < stdin.length; i++) {
-        console.log(`"${stdin[i]}": ${stdin.charCodeAt(i)}`);
+    if (!repo) {
+        throw new Error("No repo name provided");
     }
-    console.log("~~~~~~~~~~~ STDIN CHARCODES ~~~~~~~~~~~");
+    if (!token) {
+        throw new Error("No Github token provided");
+    }
 
-    const branchLabels = stdin.split("\n");
-    console.log(
-        `Stdin (split): "${branchLabels.map((label) => `"${label}",`)}"`
-    );
     const releaseBranchLabel = branchLabels.find((label) =>
         label.startsWith("release/")
     );
 
-    console.log(`Release branch label: ${releaseBranchLabel}`);
+    console.log(`Found release branch label: ${releaseBranchLabel}`);
 
     if (!releaseBranchLabel) {
-        console.error("Fail - No valid release branch label found");
-        process.exit(1); // Exit with an error
+        core.setOutput("targetBranchExists", false);
+        process.exit(0);
     }
 
-    const token = process.argv[3];
+    const branchExists = await checkBranchExistence(
+        owner,
+        repo,
+        releaseBranchLabel,
+        token
+    );
 
-    checkBranchExistence(owner, repo, releaseBranchLabel, token)
-        .then((branchExists) => {
-            if (branchExists) {
-                console.log("Success");
-            } else {
-                console.error("Fail - Release branch does not exist");
-                process.exit(1); // Exit with an error
-            }
-        })
-        .catch((error) => {
-            console.error("Fail - Error occurred", error);
-            process.exit(1); // Exit with an error
-        });
+    if (branchExists) {
+        core.setOutput("targetBranchExists", true);
+        core.setOutput("targetBranchName", releaseBranchLabel);
+    } else {
+        core.setOutput("targetBranchExists", false);
+    }
 })();
